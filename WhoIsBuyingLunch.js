bb4af192f3611a440bd86614fc639954258c48c2
SANJAY KUMAR <44132320+Trmpsanjay@users.noreply.github.com>
2020-07-04 22:45:41 +0530

    Create WhoIsBuyingLunch.js

---
+++ WhoIsBuyingLunch.js
@@ -0,0 +1,17 @@
+function whosPaying(names) {
+    
+/******Don't change the code above*******/
+    
+    //Write your code here.
+    
+    var length = names.length;
+    var random = Math.floor(Math.random()*length);
+    var ans = names[random]+" is going to buy lunch today!";
+    return ans;
+    
+    
+    
+
+
+/******Don't change the code below*******/    
+}
