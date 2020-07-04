8044c64ccab49c4f15c17efca1023659cdc4ad01
SANJAY KUMAR <44132320+Trmpsanjay@users.noreply.github.com>
2020-07-04 22:05:49 +0530

    Create LifeInWeeks.js

---
+++ LifeInWeeks.js
@@ -0,0 +1,18 @@
+function lifeInWeeks(age) {
+   
+    
+/************Don't change the code above************/    
+     var years = (90-age);
+    var month = years*12;
+    var weeks  = years*52;
+    var days = years*365;
+    console.log("You have "+days+" days, "+weeks+" weeks, and "+month+" months left.")
+    //Write your code here.
+    
+    
+    
+    
+    
+/*************Don't change the code below**********/
+}
+
