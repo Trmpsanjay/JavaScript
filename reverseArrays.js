05c9f58835b03611dd1b2820d8fa2306b5e9c1d9
SANJAY KUMAR <44132320+Trmpsanjay@users.noreply.github.com>
2021-04-13 17:38:38 +0530

    Create reverseArrays.js

---
+++ reverseArrays.js
@@ -0,0 +1,17 @@
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+var reverseString = function(s) {
+    
+    
+    // need to understand 
+   //let arr =  new Array();
+    // for(let i=s.length-1;i>=0;i--){
+    //     console.log(s[i]);
+    //     arr.push(s[i]);
+    //     console.log(arr);
+    // }
+    // return arr;
+    return s.reverse(); // with inbuilt function
+};
